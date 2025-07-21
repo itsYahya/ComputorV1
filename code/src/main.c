de9e1d60a10e4c89b856cca70454eb2fca4fe931
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-07-21 21:55:20 +0100

    chore: adding arg control

--- code/src/main.c
+++ code/src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    printf("hello world!\n");
+int main(int argc, char **argv){
+    if (argc != 2)
+    {
+        printf("computro: error.\n");
+        printf("computor: use: ./computor \"5 + 4 * X + X^2= X^2\"\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < argc; i++){
+        printf("%s\n", argv[i]);
+    }
     return (0);
 }
