b82b0e7a1fa2dcc12b4f836609e0ab22c48d4af4
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-08-08 12:56:01 +0100

    feat: update main function to fit the new setup

--- code/src/main.c
+++ code/src/main.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "parse.h"
+#include "utils.h"
+#include "computor.h"
+#include "math.h"
 
 int main(int argc, char **argv){
-    if (argc != 2)
+    int			status;
+	int			degree = 0;
+	double		res[3] = {0, 0, 0};
+	t_equation	equation;
+	
+	status = 42;
+	equation.size = 0;
+	equation.length = 0;
+	equation.components = NULL;
+    if (argc == 2)
     {
-        printf("computro: error.\n");
-        printf("computor: use: ./computor \"5 + 4 * X + X^2= X^2\"\n");
-        exit(1);
-    }
-
-    for (int i = 0; i < argc; i++){
-        printf("%s\n", argv[i]);
-    }
+        status = 0;
+        parsString(argv[1], &status, &equation);
+		if (status)
+		{
+			free(equation.components);
+			checkStatus(status);	
+		}	
+	}
+	printReducedForm(equation, res, &degree);
+	solvePolynomial(res, degree);
+	free(equation.components);
     return (0);
 }
